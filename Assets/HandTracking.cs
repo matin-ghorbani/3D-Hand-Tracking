e4db7837f9b55e1af26f5a259ff48d3cc550c5aa
Matin Ghorbani <porya.ghorbani10@gmail.com>
2024-07-07 18:30:00 +0330

    Add Unity Assets

---
+++ Assets/HandTracking.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandTracking : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public UDPReceive udpReceive;
+    public GameObject[] handPoints;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private int offset = 70;
+    void Update()
+    {
+        string data = udpReceive.data;
+        if (data.Length > 0)
+        {
+            data = data.Remove(0, 1);
+            data = data.Remove(data.Length - 1, 1);
+
+            string[] points = data.Split(',');
+            // print(points[0]);
+
+            for (int i = 0; i < 21; i++)
+            {
+                float x = 7 - float.Parse(points[i * 3]) / this.offset;
+                float y = float.Parse(points[i * 3 + 1]) / this.offset;
+                float z = float.Parse(points[i * 3 + 2]) / this.offset;
+
+                handPoints[i].transform.localPosition = new Vector3(x, y, z);
+            }
+        }
+        else
+            print("No hand detected.");
+    }
+}
